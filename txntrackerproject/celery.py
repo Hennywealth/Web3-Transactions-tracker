3ab5c64e5628d2706944022b625cdf3e10cc68ea
Eniola Kelvin <enikelvin5@gmail.com>
2022-07-31 05:17:14 +0100

    turned off celery

--- txntrackerproject/celery.py
+++ txntrackerproject/celery.py
@@ -1,25 +1,25 @@
 
-from __future__ import absolute_import, unicode_literals
-import os
-from celery import Celery
-from django.conf import settings
-from celery.schedules import crontab
+# from __future__ import absolute_import, unicode_literals
+# import os
+# from celery import Celery
+# from django.conf import settings
+# from celery.schedules import crontab
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'txntrackerproject.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'txntrackerproject.settings')
 
-app = Celery('txntrackerproject')
+# app = Celery('txntrackerproject')
 
-app.conf.enable_utc = False
+# app.conf.enable_utc = False
 
-app.conf.update(timezone='Africa/Lagos')
+# app.conf.update(timezone='Africa/Lagos')
 
-app.config_from_object(settings, namespace='CELERY')
+# app.config_from_object(settings, namespace='CELERY')
 
-# Celery Schedules - https://docs.celeryproject.org/en/stable/reference/celery.schedules.html
+# # Celery Schedules - https://docs.celeryproject.org/en/stable/reference/celery.schedules.html
 
-app.autodiscover_tasks()
+# app.autodiscover_tasks()
 
-@app.task(bind=True)
-def debug_task(self):
-    print(f'Request: {self.request!r}')
+# @app.task(bind=True)
+# def debug_task(self):
+#     print(f'Request: {self.request!r}')
     
